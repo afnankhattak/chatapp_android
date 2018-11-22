02d1eb0e9d6495c63a44b5a6b45bc782d5e82bb4
afnankhattak <hmafnan3@gmail.com>
2018-11-22 23:39:11 +0500

    Adding RealTime Database to chatapp

--- app/src/main/java/com/example/haier/chatapp/Fragments/ChatFragment.java
+++ app/src/main/java/com/example/haier/chatapp/Fragments/ChatFragment.java
@@ -2,8 +2,10 @@ package com.example.haier.chatapp.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +13,13 @@ import android.widget.ImageView;
 
 import com.example.haier.chatapp.Models.Author;
 import com.example.haier.chatapp.Models.Message;
+import com.example.haier.chatapp.Models.SMS;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 import com.stfalcon.chatkit.commons.ImageLoader;
 import com.stfalcon.chatkit.messages.MessageInput;
@@ -30,7 +37,8 @@ public class ChatFragment extends Fragment {
     private MessagesList messagesList;
 
     private Author currentUser;
-
+    FirebaseDatabase database ;
+    DatabaseReference myRef;
     public ChatFragment() {
         // Required empty public constructor
     }
@@ -41,7 +49,8 @@ public class ChatFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_chat, container, false);
-
+         database = FirebaseDatabase.getInstance();
+         myRef = database.getReference();
         messageInput = (MessageInput) view.findViewById(R.id.input);
         messagesList = (MessagesList) view.findViewById(R.id.messagesList);
 
@@ -53,20 +62,40 @@ public class ChatFragment extends Fragment {
             }
         };
 
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        currentUser = new Author(firebaseUser.getUid(), firebaseUser.getDisplayName());
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        currentUser = new Author(firebaseUser.getUid(), firebaseUser.getEmail());
 
         final MessagesListAdapter<Message> adapter = new MessagesListAdapter<>(currentUser.getId(), imageLoader);
         messagesList.setAdapter(adapter);
 
         messageInput.setInputListener(new MessageInput.InputListener() {
             @Override
-            public boolean onSubmit(CharSequence input) {
+            public boolean onSubmit(final CharSequence input) {
 
-                Message message = new Message(input.toString(), currentUser, new Date());
 
-                adapter.addToStart(message, true);
 
+                SMS sms=new SMS(firebaseUser.getUid(),input.toString(), currentUser, new Date());
+
+
+                myRef.child("afnan_chat").push().setValue(sms).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Message message = new Message(firebaseUser.getUid(),input.toString(), currentUser, new Date());
+                        adapter.addToStart(message, true);
+
+                        //Log.e("Message",aVoid.toString());
+                        // Write was successful!
+                        // ...
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Write failed
+                           //     Log.e("Message",e.getMessage());
+
+                                // ...
+                            }
+                        });
                 return true;
             }
         });
--- app/src/main/java/com/example/haier/chatapp/Models/SMS.java
+++ app/src/main/java/com/example/haier/chatapp/Models/SMS.java
@@ -1,17 +1,26 @@
 package com.example.haier.chatapp.Models;
 
-import com.stfalcon.chatkit.commons.models.IMessage;
-
 import java.util.Date;
 
-public class Message implements IMessage {
+/**
+ * Created by haier on 11/22/2018.
+ */
+
+
+
+
+public class SMS {
 
     private String id;
     private String text;
     private Author author;
     private Date createdAt;
 
-    public Message(String id, String text, Author author, Date createdAt) {
+    public SMS(){
+
+    }
+
+    public SMS(String id, String text, Author author, Date createdAt) {
 
         //todo: set message id
         this.id =id;
@@ -20,22 +29,37 @@ public class Message implements IMessage {
         this.createdAt = createdAt;
     }
 
-    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
     public String getId() {
         return id;
     }
 
-    @Override
+
     public String getText() {
         return text;
     }
 
-    @Override
+
     public Author getUser() {
         return author;
     }
 
-    @Override
+
     public Date getCreatedAt() {
         return createdAt;
     }
